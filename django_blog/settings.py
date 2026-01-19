3e1d09abf44e44e75df0edb8c2e8150c872bf724
Sascha Klement <sascha.r.klement@gmail.com>
2026-01-19 19:35:42 +0100

    Configure environment variables for local and Heroku deployment

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -27,7 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8y7qv#z55fuo5!1-qzp2xso6)%ug7-yv4!pddg9bq!io#64q#d'
+
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
