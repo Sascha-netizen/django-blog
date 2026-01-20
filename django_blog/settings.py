d4aa16a84e22fee5b894968124ce58546e6fd46e
Sascha Klement <sascha.r.klement@gmail.com>
2026-01-20 19:39:22 +0100

    Add optional excerpt field to post model

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'django-blog-one-d6ac77d97991.herokuapp.com',
