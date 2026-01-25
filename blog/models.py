eec98cc331c12c26d38f8a364a6b33578f0585f3
Sascha Klement <sascha.r.klement@gmail.com>
2026-01-25 12:29:16 +0100

    Add meta class to comment model for odering and display

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
+
+# Post model:
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -23,6 +26,7 @@ class Post(models.Model):
         return f"The title of this post is '{self.title}' | written by {self.author}"
 
 
+#  Comment model:
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments"
@@ -34,4 +38,8 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
-
+    class Meta:
+        ordering = ["created_on"]
+    
+    def __str__(self):
+        return f"Comment '{self.body}' | by {self.author}"
