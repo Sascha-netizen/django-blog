df6ee87e6fb16a41a3ce938875f0c8dde8f68cbb
Sascha Klement <sascha.r.klement@gmail.com>
2026-02-03 16:40:47 +0100

    Add About model migrations and register ABout model in admin

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import About
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(About)
+class PostAbout(SummernoteModelAdmin):
+     summernote_fields = ('title', 'content',)
+
 
 # Register your models here.
+
