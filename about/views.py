18af765b876aeccc976ca14384638aec25da4499
Sascha Klement <sascha.r.klement@gmail.com>
2026-02-10 20:29:12 +0100

    Add post handler to collaboration request form

--- about/views.py
+++ about/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from .models import About
 from .forms import CollaborateForm
+from django.contrib import messages 
 
-# Create your views here.
+
 def about(request):
     about_entry = About.objects.first()
-    collaborate_form = CollaborateForm()
-    context = { "about": about_entry, "collaborate": collaborate_form, }
 
-    return render(
-        request,
-        "about/about.html",
-        context,
-    )
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.success(
+                request,
+                "Collaboration request received! I endeavour to respond within 2 working days."
+            )
+            # reset form after successful submission
+            collaborate_form = CollaborateForm()
+    else:
+        # For GET requests, show a blank form
+        collaborate_form = CollaborateForm()
+
+    context = { 
+        "about": about_entry,
+        "collaborate_form": collaborate_form,
+    }
+
+    return render(request, "about/about.html", context)
