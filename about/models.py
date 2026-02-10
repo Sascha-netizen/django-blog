b3709084646b5a31b273f81e1b36f4a4e881f440
Sascha Klement <sascha.r.klement@gmail.com>
2026-02-10 19:44:41 +0100

    Add collaboration request form to about-me page

--- about/models.py
+++ about/models.py
@@ -4,8 +4,18 @@ from django.db import models
 
 class About(models.Model):
     title = models.CharField(max_length=300)
+    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f" {self.title} "
+    
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=250)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request form {self.name}"
--- about/views.py
+++ about/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 def about(request):
     about_entry = About.objects.first()
-    context = { "about": about_entry }
+    collaborate_form = CollaborateForm()
+    context = { "about": about_entry, "collaborate": collaborate_form, }
 
     return render(
         request,
